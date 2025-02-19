7c9b87a73348741423e969342d5b4b86d33cfa99
Aaron Rivelino Vargas Kononenko <aaron.rivelino.vargas-kononenko@stu.ocsb.ca>
2025-02-19 17:48:57 +0000

    areaPerRect.cpp

---
+++ areaPerRect.cpp
@@ -0,0 +1,12 @@
+//  Copyright (2025) Aaron Rivelino All rights reserved.
+// Created By : Aaron Rivelino
+// ate : Feb 15, 2025
+// The code calculates the area of a rectangle
+
+#include <cmath>
+#include <iostream>
+
+int main() {
+    std::cout << "Area, length 5, width 13.23 ="<< (5*13.23) << std::endl;
+    std::cout << "Perimeter, L 167, W 30 =" << (167*2+30*2) << std::endl;
+}
